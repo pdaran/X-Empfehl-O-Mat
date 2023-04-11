37eecd4dd3906bd0a823fe026664635ba48d00fc
gitDaran <paundradaran@gmail.com>
2023-04-11 23:00:50 +0200

    abschnitt 4 fertig

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,26 @@
 # frozen_string_literal: true
 
+
+
 Rails.application.routes.draw do
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
   # Defines the root path route ("/")
+
   root 'categories#index'
+
   resources :articles do
+
     resources :comments
+
   end
+
   resources :categories do
+
     resources :products
+
   end
+
 end
+
--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,9 +1,18 @@
 # frozen_string_literal: true
 
+
+
 require 'test_helper'
 
+
+
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
+
   # test "the truth" do
+
   #   assert true
+
   # end
+
 end
+
