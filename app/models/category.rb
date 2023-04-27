2f245f1f4b7007e4725ef69b562078f8dbb528fa
gitDaran <paundradaran@gmail.com>
2023-04-27 15:56:55 +0200

    ✨ Category list as active record

---
+++ app/models/category.rb
@@ -0,0 +1,3 @@
+class Category < ApplicationRecord
+    validates :title, presence: true, length: { minimum: 3 }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_14_101853) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_27_123200) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "comments", force: :cascade do |t|
