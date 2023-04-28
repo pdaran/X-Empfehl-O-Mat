4e50195e7402e8e492d32bf84d0b8d241c6dcd5f
gitDaran <paundradaran@gmail.com>
2023-04-28 13:14:22 +0200

    🎨 CRUD for categories-site

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -15,10 +15,31 @@ class CategoriesController < ApplicationController
     @input = category_params
 
     if @category.save
-      redirect_to @category
+      redirect_to action: "index"
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+
+    if @category.update(category_params)
+      redirect_to @category
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+
+    redirect_to action: "index", status: :see_other
   end
 
   private
