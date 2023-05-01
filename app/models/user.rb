78c070e1d15186e3a2d331b54e527bc68419ce88
sebastian <sebastian.menne@alumni.fh-aachen.de>
2023-05-01 16:02:21 +0000

    Added validation for email in user model, added has_secure_password for password handling

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
+# email:string
+# password_digest:string
+#
+# password:string virtual
+# password_confirmation:string virtual
+
 class User < ApplicationRecord
+  has_secure_password
+
+  # Verify that email field is not blank and that it doesn't already exist in the db (prevents duplicates):
+  validates :email, presence: true, uniqueness: true
 end
