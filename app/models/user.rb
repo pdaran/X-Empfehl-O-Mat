0f680b0ef245339d33dd9d70b188b84f54eb27da
sebastian <sebastian.menne@alumni.fh-aachen.de>
2023-05-01 16:13:50 +0000

    Added further validation for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,5 @@ class User < ApplicationRecord
   has_secure_password
 
   # Verify that email field is not blank and that it doesn't already exist in the db (prevents duplicates):
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address" }
 end
