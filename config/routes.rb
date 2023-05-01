192b954d4c529be5b426db1da179c9a5c6d78545
sebastian <sebastian.menne@alumni.fh-aachen.de>
2023-05-01 16:20:24 +0000

    Added registrations controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get "sign_up", to: "registrations#new"
   # Defines the root path route ("/")
   # root "articles#index"
 end
