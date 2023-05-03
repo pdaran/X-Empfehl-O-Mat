2878972bc5c36ad1642079a1ff8208782582c475
Marvin Doushan <marvin.doushan@alumni.fh-aachen.de>
2023-05-03 10:44:06 +0200

    combined routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'recommender', to: 'recommender#category'
+  get 'recommender/:id', to: 'recommender#articles'
+  post 'recommender/:id', to: 'recommender#articles'
+  get 'result', to: 'recommender#result'
+  get 'homepage/index'
+  root "homepage#index"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   get '/dashboards', to: 'dashboards#index'
-  root 'categories#index'
+  #root 'categories#index'
 
   resources :articles do
     resources :comments
