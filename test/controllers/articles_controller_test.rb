5af445d7e01bc8a7bb1996f590abece9483d5c15
gitDaran <paundradaran@gmail.com>
2023-05-03 15:48:39 +0200

    💄 styling fixes

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,37 +1,11 @@
 # frozen_string_literal: true
 
-
-
 require 'test_helper'
-
-
-
-
-
-
-
-require 'test_helper'
-
-
-
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
-
-
-
   # test "the truth" do
-
-
 
   #   assert true
 
-
-
   # end
-
-
-
 end
-
-
-
