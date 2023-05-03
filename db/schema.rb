0793b5033b925780445387e25a73239cf89ddb0b
Marvin Doushan <marvin.doushan@alumni.fh-aachen.de>
2023-05-03 10:45:34 +0200

    combined schema

--- db/schema.rb
+++ db/schema.rb
@@ -45,6 +45,24 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_28_114740) do
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
+  create_table "customers", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.boolean "like"
+    t.integer "customer_id", null: false
+    t.integer "product_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_likes_on_customer_id"
+    t.index ["product_id"], name: "index_likes_on_product_id"
+  end
+
+  add_foreign_key "likes", "customers"
+  add_foreign_key "likes", "products"
   add_foreign_key "comments", "articles"
   add_foreign_key "products", "categories"
 end
