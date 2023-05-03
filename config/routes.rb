69893b2e2c442dc452efd898398259a325355cf9
Marvin Doushan <marvin.doushan@alumni.fh-aachen.de>
2023-05-03 10:44:06 +0200

    fixed a link and added necessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'recommender', to: 'recommender#category'
+  get 'recommender/:id', to: 'recommender#articles'
+  post 'recommender/:id', to: 'recommender#articles'
+  get 'result', to: 'recommender#result'
   get 'homepage/index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
   root "homepage#index"
 end
