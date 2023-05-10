3a7c0134a1dd3b4c58095bd4fa87345f4170ca4e
Marvin Doushan <marvin.doushan@alumni.fh-aachen.de>
2023-05-10 23:33:55 +0200

    chaged application_controller.rb to fix routing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,9 +9,18 @@ class ApplicationController < ActionController::Base
     { locale: set_locale }
   end
 
-  def switch_locale(&)
+  # fixes category_path and similar functions and forms not adding the locale subdir
+  def default_url_options
+    { locale: set_locale }
+  end
+
+  def switch_locale(&action)
     locale = params[:locale] || I18n.default_locale
-    I18n.with_locale(locale, &)
+    I18n.with_locale(locale, &action)
+  end
+
+  private def set_locale
+    params[:locale] || I18n.default_locale
   end
 
   private
