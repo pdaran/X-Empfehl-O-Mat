cd42f84faba8677899d01b4ea0944e19d35c55d3
sebastian <sebastian.menne@alumni.fh-aachen.de>
2023-05-10 17:40:14 +0000

    Migration after merge from main

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_03_075154) do
-  create_table "users", force: :cascade do |t|
-    t.string "email", null: false
-    t.string "password_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ActiveRecord::Schema[7.0].define(version: 2023_05_03_065154) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -66,6 +59,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_03_075154) do
     t.datetime "updated_at", null: false
     t.string "status"
     t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "comments", "articles"
