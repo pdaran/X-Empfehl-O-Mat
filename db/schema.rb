be327db43bd0a22a09707d6ddc227f98526e53a2
sebastian <sebastian.menne@alumni.fh-aachen.de>
2023-05-18 19:58:11 +0000

    Added Columns for roles to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_03_065154) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_17_114740) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -66,6 +66,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_03_065154) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
+    t.boolean "shop", default: false
   end
 
   add_foreign_key "comments", "articles"
