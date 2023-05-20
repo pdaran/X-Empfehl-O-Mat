08c971cb0e5039853db285fd5e6cdf808b3afe4a
gitDaran <paundradaran@gmail.com>
2023-05-20 19:58:37 +0200

    ⚰️ 🎨 ✨ translations done, image preview

---
+++ app/javascript/file_preview.js
@@ -0,0 +1,24 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  const fileInput = document.getElementById("file-input");
+  const previewContainer = document.getElementById("file-preview");
+
+  fileInput.addEventListener("change", (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+
+      reader.addEventListener("load", () => {
+        const previewImage = document.createElement("img");
+        previewImage.src = reader.result;
+        previewImage.classList.add("preview-image"); // Add this line
+        previewContainer.innerHTML = ""; // Clear previous contents
+        previewContainer.appendChild(previewImage);
+      });
+
+      reader.readAsDataURL(file);
+    } else {
+      previewContainer.innerHTML = ""; // Clear the preview container
+    }
+  });
+});
