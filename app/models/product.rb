950ee24854cd18c0d14a2d5bee96d5342fecd18b
gitDaran <paundradaran@gmail.com>
2023-05-26 12:23:22 +0200

    💄 ⚡️ img preview improvement, prod & form styling

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,7 +6,9 @@ class Product < ApplicationRecord
   belongs_to :category
 
   has_one_attached :image do |attachable|
-    attachable.variant :thumbnail, resize_to_limit: [250, 250]
+    attachable.variant :large, resize_to_fill: [250, 250]
+    attachable.variant :thumbnail, resize_to_fill: [56, 56]
+    attachable.variant :medium, resize_to_limit: [100, 100]
   end
 
   validates :product, presence: true, length: { minimum: 3 }
