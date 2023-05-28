f68976f40d0fdd0981eb9c98cfa7a17a3222bf50
sebastian <sebastian.menne@alumni.fh-aachen.de>
2023-05-28 11:36:56 +0000

    Very basic get request test.

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
 class HomepageController < ApplicationController
-  def index; end
+  def index;
+      require 'net/http'
+      require "uri"
+
+      uri = URI('http://empfehl-flask:8000/recommend')
+
+      response = Net::HTTP.get(uri)
+      @empfehlung = response
+
+  end
   # static pages
   def about; end
 
