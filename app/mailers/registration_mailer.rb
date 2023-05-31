fda72d82a3899ac37a20789fe1cacb24be3214d6
gitDaran <paundradaran@gmail.com>
2023-05-31 14:34:11 +0200

    🚨 make rubocop happy

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -1,15 +1,17 @@
+# frozen_string_literal: true
+
 class RegistrationMailer < ApplicationMailer
-    layout 'mailer' # use mailer.(html|text).erb as the layout
-    default from: email_address_with_name('notification@example.com', 'Example Company Notifications')
-  
-    def signup_email
-      @user = params[:user]
-      @url  = 'http://example.com/login'
-      mail(
-        to: email_address_with_name(@user.email, 'Example User'),
-        subject: 'Registration Confirmation',
-        template_path: 'registration_mailer',
-        template_name: 'signup_email'
-      )
-    end
+  layout 'mailer' # use mailer.(html|text).erb as the layout
+  default from: email_address_with_name('notification@example.com', 'Example Company Notifications')
+
+  def signup_email
+    @user = params[:user]
+    @url  = 'http://example.com/login'
+    mail(
+      to: email_address_with_name(@user.email, 'Example User'),
+      subject: 'Registration Confirmation',
+      template_path: 'registration_mailer',
+      template_name: 'signup_email'
+    )
+  end
 end
