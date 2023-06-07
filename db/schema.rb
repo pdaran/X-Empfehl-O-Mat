b613d8c0d40b5498b8dcfb79f5807dce17efbd4f
farhansasono <farhanshafysasono@gmail.com>
2023-06-07 19:45:52 +0200

    New Branch für Shop

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_01_092235) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_04_144401) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_01_092235) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "status"
+  end
+
   create_table "attrs", force: :cascade do |t|
     t.string "name"
     t.string "unit"
@@ -59,6 +67,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_01_092235) do
     t.string "status"
     t.bigint "shop_id", null: false
     t.index ["shop_id"], name: "index_categories_on_shop_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.bigint "article_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "status"
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
   create_table "customers", force: :cascade do |t|
@@ -98,6 +116,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_01_092235) do
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
+  create_table "shop_registrations", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "shop_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["shop_id"], name: "index_shop_registrations_on_shop_id"
+    t.index ["user_id"], name: "index_shop_registrations_on_user_id"
+  end
+
   create_table "shops", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -105,6 +132,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_01_092235) do
     t.text "address"
     t.string "phone_no"
     t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "shops2", force: :cascade do |t|
+    t.string "name"
+    t.string "adresse"
+    t.string "kontaktperson"
+    t.bigint "telefonnummer"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -121,10 +159,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_01_092235) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "attrs", "categories"
-  add_foreign_key "categories", "shops"
+  add_foreign_key "categories", "shops2", column: "shop_id"
+  add_foreign_key "comments", "articles"
   add_foreign_key "likes", "customers"
   add_foreign_key "likes", "products"
   add_foreign_key "product_attrs", "attrs"
   add_foreign_key "product_attrs", "products"
   add_foreign_key "products", "categories"
+  add_foreign_key "shop_registrations", "shops2", column: "shop_id"
+  add_foreign_key "shop_registrations", "users"
 end
