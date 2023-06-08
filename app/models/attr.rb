781098d56b2b0b398ccd8519ed3b453168299353
Marvin Doushan <marvin.doushan@alumni.fh-aachen.de>
2023-06-08 17:33:13 +0200

    add validations to attr

--- app/models/attr.rb
+++ app/models/attr.rb
@@ -5,4 +5,13 @@ class Attr < ApplicationRecord
   belongs_to :category
 
   enum attrtype: { bool: 'bool', numeric: 'numeric', text: 'text' }
+  enum status: { active: 'active', deactivated: 'deactivated' }
+
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :unit, presence: true, if: :numeric?
+  validates :unit, absence: true, unless: :numeric?
+
+  def numeric?
+    attrtype == 'numeric'
+  end
 end
