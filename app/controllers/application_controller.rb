791b3eb2fad396a0687d359afb5298fbb63c6c7e
farhansasono <farhanshafysasono@gmail.com>
2023-06-08 21:57:27 +0200

    Switchlocale update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
 
   around_action :switch_locale
   before_action :set_current_user
+  before_action :set_current_shop
   before_action :turbo_frame_request_variant
 
   # fixes category_path and similar functions and forms not adding the locale subdir
@@ -12,14 +13,24 @@ class ApplicationController < ActionController::Base
     { locale: I18n.locale }
   end
 
-  def switch_locale(&)
+  def switch_locale(&action)
     locale = params[:locale] || I18n.default_locale
-    I18n.with_locale(locale, &)
+    available_locales = I18n.available_locales.map(&:to_s)
+    if available_locales.include?(locale)
+      I18n.with_locale(locale, &action)
+    else
+      I18n.with_locale(I18n.default_locale, &action)
+    end
   end
+  
 
   def pundit_user
     Current.user
   end
+
+  def pundit_shop
+    Current.shop
+  end 
 
   private
 
@@ -37,6 +48,11 @@ class ApplicationController < ActionController::Base
     Current.user = User.find_by(id: session[:user_id])
   end
 
+  def set_current_shop
+    return unless session[:shop_id]
+    Current.shop = Shop.find_by(id: session[:shop_id])
+  end
+
   def require_user_logged_in!
     return unless Current.user.nil?
 
@@ -44,11 +60,11 @@ class ApplicationController < ActionController::Base
                 alert: t('session.nologin')
   end
 
-  def require_user_shop!
-    return if Current.user.shop?
+  def require_shop_logged_in!
+    return unless Current.shop.nil?
 
-    redirect_to session[:user_id] && sign_in_path,
-                alert: t('session.noperm')
+    redirect_to root_path,
+                alert: t('session.nologin')
   end
 
   def require_user_admin!
