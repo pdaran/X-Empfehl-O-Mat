7b82ca330775ded08476d3988ed79ef14d53c0d8
farhansasono <farhanshafysasono@gmail.com>
2023-06-08 22:51:51 +0200

    Delete unuse schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_04_144401) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_08_205026) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -116,15 +116,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_04_144401) do
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
-  create_table "shop_registrations", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "shop_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["shop_id"], name: "index_shop_registrations_on_shop_id"
-    t.index ["user_id"], name: "index_shop_registrations_on_user_id"
-  end
-
   create_table "shops", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -132,17 +123,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_04_144401) do
     t.text "address"
     t.string "phone_no"
     t.string "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "shops2", force: :cascade do |t|
-    t.string "name"
-    t.string "adresse"
-    t.string "kontaktperson"
-    t.bigint "telefonnummer"
-    t.string "email"
-    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -159,13 +139,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_04_144401) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "attrs", "categories"
-  add_foreign_key "categories", "shops2", column: "shop_id"
   add_foreign_key "comments", "articles"
   add_foreign_key "likes", "customers"
   add_foreign_key "likes", "products"
   add_foreign_key "product_attrs", "attrs"
   add_foreign_key "product_attrs", "products"
   add_foreign_key "products", "categories"
-  add_foreign_key "shop_registrations", "shops2", column: "shop_id"
-  add_foreign_key "shop_registrations", "users"
 end
