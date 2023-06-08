6800021e64e654d00a89a14d4d1c443c8ae2f769
farhansasono <farhanshafysasono@gmail.com>
2023-06-08 22:01:16 +0200

    Routes, Index/Root_path, Lokalisierungsupdate

--- config/routes.rb
+++ config/routes.rb
@@ -16,21 +16,64 @@ Rails.application.routes.draw do
     get '/impressum', to: 'homepage#impressum', as: 'impressum'
     get '/privacy', to: 'homepage#privacy', as: 'privacy'
     get '/contact', to: 'homepage#contact', as: 'contact'
+    get 'registrations', to: 'registrations#index', as: 'registrations'
+    get 'sessions', to: 'sessions#index', as: 'sessions'
 
-    resources :registrations, only: %i[new create] do
+
+    resources :registrations, only: %i[new, create] do
+      collection do
+        get :new_user
+        post :create_user
+      end
+    end
+    resources :registrations, only: %i[new ,create] do
       collection do
         get :new_shop
         post :create_shop
       end
     end
 
-    get 'sign_in', to: 'sessions#new'
-    post 'sign_in', to: 'sessions#create'
+    resources :sessions, only: %i[new ,create] do
+      collection do
+        get :new_shop
+        post :create_shop
+      end
+    end
+
+    resources :sessions, only: %i[new ,create] do
+      collection do
+        get :new_user
+        post :create_user
+      end
+    end
+
+    resources :passwords, only: [] do
+      collection do
+        get :edit_user
+        patch :update_pass_user
+      end
+    end
+
+    resources :passwords, only: [] do
+      collection do
+        get :edit_shop
+        patch 'password', to: 'passwords#update_pass_shop'
+      end
+    end
+    
+
+    get 'sign_in', to: 'sessions#new_user'
+    post 'sign_in', to: 'sessions#create_user'
+
+    get 'sign_in_shop', to: 'sessions#new_shop'
+    post 'sign_in_shop', to: 'sessions#create_shop'
 
     get 'password', to: 'passwords#edit', as: :edit_password
     patch 'password', to: 'passwords#update'
 
-    delete 'logout', to: 'sessions#destroy'
+    delete 'logout', to: 'sessions#destroy_user'
+
+    delete 'logout_shop', to: 'sessions#destroy_shop'
 
     get '/dashboards', to: 'dashboards#index'
 
