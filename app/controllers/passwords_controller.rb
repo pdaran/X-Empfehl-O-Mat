b9721a32a524f9f76a1cd1e123d64e6de0023fc9
gitDaran <paundradaran@gmail.com>
2023-06-15 21:11:52 +0200

    ⚡️ ⚰️ shop reset password mailer, dead code, i18n

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -4,30 +4,6 @@ class PasswordsController < ApplicationController
   def edit_user; end
   def edit_shop; end
 
-  def reset
-    @user = User.find_by(password_reset_token: params[:token])
-    @shop = Shop.find_by(password_reset_token: params[:token])
-
-    if @user || @shop
-      render :reset
-    else
-      redirect_to root_path, alert: t('password.token_invalid')
-    end
-  end
-
-  def send_reset
-    @user = User.find_by(password_reset_token: params[:token])
-    @shop = Shop.find_by(password_reset_token: params[:token])
-
-    if @user&.authenticate_reset_token(params[:token])
-      update_password(@user)
-    elsif @shop&.authenticate_reset_token(params[:token])
-      update_password(@shop)
-    else
-      redirect_to root_path, alert: t('password.token_invalid')
-    end
-  end
-
   def update_user
     if Current.user.update(password_params_user)
       redirect_to root_path, notice: t('password.edit')
@@ -45,21 +21,6 @@ class PasswordsController < ApplicationController
   end
 
   private
-
-  def update_password(resource)
-    if password_reset_expired?(resource)
-      redirect_to root_path, alert: t('password.token_invalid')
-    elsif resource.update(password: params[:password], password_confirmation: params[:password_confirmation])
-      redirect_to root_path, notice: t('password.reset_success')
-    else
-      flash.now[:alert] = t('password.error')
-      render :reset
-    end
-  end
-
-  def password_reset_expired?(resource)
-    resource.password_reset_sent_at < 2.hours.ago
-  end
 
   def password_params_user
     params.require(:user).permit(:password, :password_confirmation)
--- app/mailers/password_reset_mailer.rb
+++ app/mailers/password_reset_mailer.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 class PasswordResetMailer < ApplicationMailer
-  def password_reset_email(user_or_shop)
-    @user_or_shop = user_or_shop
-    @reset_password_url = edit_reset_password_url(token: @user_or_shop.password_reset_token, locale: I18n.locale)
-    mail(to: @user_or_shop.email, subject: 'Reset Your Password')
+  def forgot_password(shop)
+    @shop = shop
+    @greeting = 'Hello '
+    mail(
+      to: shop.email, 
+      subject: 'Reset password instructions')
   end
 end
