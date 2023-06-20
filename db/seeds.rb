06f70b1e3f7b4dfc3b9ac23df86103bc856478c8
sebastian <sebastian.menne@alumni.fh-aachen.de>
2023-06-20 16:45:23 +0000

    Remove HardCoded id from seeder, now throws error if not successful

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,9 @@
 
 User.create(email: ENV.fetch("INITIAL_ADMIN_EMAIL"), password: ENV.fetch("INITIAL_ADMIN_PASSWORD"), admin: true)
 
-s = Shop.create(name: "DevShop", email: ENV.fetch("INITIAL_ADMIN_EMAIL"), password_digest: ENV.fetch("INITIAL_ADMIN_PASSWORD"), address: "abcdefg", phone_no: "0123456789", status: "active")
-s.save
+shop = Shop.create!(name: "DevShop", email: ENV.fetch("INITIAL_ADMIN_EMAIL"), password: ENV.fetch("INITIAL_ADMIN_PASSWORD"), address: "abcdefg", phone_no: "012345678912", status: "active")
 
-c = s.categories.build(title: "TestKategorie", status: :active)
-c.save
+category = shop.categories.create!(title: "TestKategorie", status: :active)
 
-c.products.build(product: "Test Produkt", desc: "Test Beschreibung Nummer 1", status: :public, category_id: 1).save
-c.products.build(product: "Anderes Produkt", desc: "Andere Beschreibung Nummer 2", status: :public, category_id: 1).save
+category.products.create!(product: "Test Produkt", desc: "Test Beschreibung Nummer 1", status: :public)
+category.products.create!(product: "Anderes Produkt", desc: "Andere Beschreibung Nummer 2", status: :public)
