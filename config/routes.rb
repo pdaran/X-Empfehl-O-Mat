c9f72ac0cb58e654819e93227a431241af443b67
sebastian <sebastian.menne@alumni.fh-aachen.de>
2023-06-21 22:42:35 +0000

    Add default hostname for email

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  default_url_options :host => ENV.fetch("HOSTNAME")
   get 'password_resets/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: redirect("/#{I18n.default_locale}"), as: :redirected_root
